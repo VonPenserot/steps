4159f91309abc5d3f67a68f4cc01765ba71c749f
Joachim da Silva Penserot <joachim.silva.penserot@gmail.com>
2023-06-26 23:14:22 -0400

    created components

--- src/App.js
+++ src/App.js
@@ -35,25 +35,48 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            <Button
               onClick={handlePrevious}
+              bgColor="#7950f2"
+              textColor="#fff"
+              text="Back"
             >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              <span>👈</span> Previous
+            </Button>
+            <Button
               onClick={handleNext}
+              bgColor="#7950f2"
+              textColor="#fff"
+              text="Next"
             >
-              Next
-            </button>
+              Next <span>👉</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
